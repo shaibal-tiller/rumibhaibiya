e1efe3ad07b43517b4e1c57758b4435f4bc98bb7
shaibal-tiller <shaibal.tiller@gmail.com>
2022-11-20 13:06:40 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 
 function App() {
   const calculateTimeLeft = () => {
-    const difference = +new Date("2022-02-28T18:20:00+05:30") - +new Date();
+    const difference = +new Date("2022-12-31T18:20:00+05:30") - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
       timeLeft = {
-        hours: Math.floor(difference / (1000 * 60 * 60)),
+        days: Math.floor(difference / (1000 * 60 * 60)/24),
+        hours: Math.floor(difference / (1000 * 60 * 60)%24),
         minutes: Math.floor((difference / 1000 / 60) % 60),
         seconds: Math.floor((difference / 1000) % 60),
       };
@@ -26,15 +27,26 @@ function App() {
   });
 
   return (
-    <div className="App">
+    <div className="App text-5xl bg-black h-[100vh]  flex items-center justify-center">
       {timeLeft.hours || timeLeft.minutes || timeLeft.seconds ? (
-        <p>
+        <div>
+          <p className="text-white text-xs">"আর কতকাল একা থাকবে রুমি ভাই" পরিষদ</p>
+          <h1 className="text-red-500">আর বাকি</h1>
+        <hr/>
+        <br/>
+        <p className="text-red-500 bg-black">
+          <span >{timeLeft.days}</span>
+          <span> Days </span>
           <span>{timeLeft.hours}</span>
-          <span>:</span>
+          <span> Hours </span>
           <span>{timeLeft.minutes}</span>
-          <span>:</span>
+          <span> Minutes </span>
           <span>{timeLeft.seconds}</span>
+          <span> Seconds </span>
         </p>
+        <br/>
+        <hr/>
+        </div>
       ) : (
         <p>Time is up 🔥</p>
       )}
