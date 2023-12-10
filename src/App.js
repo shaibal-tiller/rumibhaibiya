d8aa9fff674d8f0d0f24b047c47103c2334ab68d
shaibal-tiller <114130885+shaibal-tiller@users.noreply.github.com>
2023-12-10 10:48:09 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   const calculateTimeLeft = () => {
-    const difference = +new Date("2023-11-15") - +new Date();
+    const difference = +new Date("2023-12-14") - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
